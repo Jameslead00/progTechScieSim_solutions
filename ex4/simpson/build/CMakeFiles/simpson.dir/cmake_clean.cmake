3f85038e18d5a7e8d7e1904f848a43b365f62056
jameslead <jleadbeater@student.ethz.ch>
2023-08-07 00:10:27 +0200

    simpson done

---
+++ ex4/simpson/build/CMakeFiles/simpson.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/simpson.dir/main.cpp.o"
+  "CMakeFiles/simpson.dir/main.cpp.o.d"
+  "simpson"
+  "simpson.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/simpson.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
