c9b0df258d5eb128dc1ace91ced4eefef34606c3
jameslead <jleadbeater@student.ethz.ch>
2023-08-06 23:32:29 +0200

    z2 done

--- ex4/z2_group_skeleton.cpp
+++ ex4/z2_group_skeleton.cpp
@@ -11,33 +11,51 @@ template<class T>
 T identity_element() { return T(1); }
 
 // to be implemented: specialize the previous function for Z2
+//template specializatuon for Z2
+template<>
+Z2 identity_element(){return Plus;}
+
 
 Z2 operator*(const Z2 a, const Z2 b)
 {
-  // to be implemented
+  // to be implemented (+*+=-*-=+; +*-=-*+=-)
+  return (a==b ? Plus : Minus);
 }
 
 std::ostream& operator<<(std::ostream& os, const Z2 a)
 {
   // to be implemented
+  os << (a==Plus ? "Plus" : "Minus");
+  return os;
 }
 
 template<class T>
 T operator*(const T a, const Z2 b)
 {
   // to be implemented
+  return (b==Plus ? a : -a);
 }
 
 template<class T>
 T operator*(const Z2 b, const T a)
 {
   // to be implemented
+  return (a * b);
+
 }
 
 template<class T>
 T mypow(T a, unsigned int n)
 {
   // to be implemented
+  T result(identity_element<T>());
+  while(n > 0){
+    if(n%2 != 0){
+      result = result * a;
+      }
+      n /= 2;
+      a = a * a;
+  } return result;
 }
 
 int main()
