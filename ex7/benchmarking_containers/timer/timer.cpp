cfd07c582a2bad442093524ded125e1c8726601d
jameslead <jleadbeater@student.ethz.ch>
2023-08-20 22:01:13 +0200

    benchmarking containers done

---
+++ ex7/benchmarking_containers/timer/timer.cpp
@@ -0,0 +1,17 @@
+#include "timer.hpp"
+
+Timer::Timer() {
+  tstart_ = std::chrono::high_resolution_clock::now();
+}
+
+void Timer::start() {
+  tstart_ = std::chrono::high_resolution_clock::now();
+}
+
+void Timer::stop() {
+  tend_ = std::chrono::high_resolution_clock::now();
+}
+
+double Timer::duration() const {
+  return std::chrono::duration< double >(tend_ - tstart_).count();
+}
---
+++ ex7/benchmarking_containers/timer/timer.hpp
@@ -0,0 +1,19 @@
+#ifndef TIMER_HPP
+#define TIMER_HPP
+
+#include <chrono>
+    
+class Timer {
+public:
+  Timer();
+  void start();
+  void stop();
+  double duration() const;
+
+private:
+  using time_point_t = std::chrono::high_resolution_clock::time_point;
+  time_point_t tstart_;
+  time_point_t tend_;
+};
+
+#endif // !defined TIMER_HPP
